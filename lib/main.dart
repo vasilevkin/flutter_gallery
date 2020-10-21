1a4131fe85b2922c6fd12af5ba9235c3973300ad
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-10-21 12:15:05 +0300

    Move FlutterGalleryApp to the separate file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'app/app.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Gallery',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Text('Flutter Gallery'),
-    );
-  }
+void main() {
+  runApp(FlutterGalleryApp());
 }
